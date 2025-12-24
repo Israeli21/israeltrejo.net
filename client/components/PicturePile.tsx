e967ed1b3c24dda6ce0e17a515092e7143d06a04
Israel Trejo <isrtrejo07@gmail.com>
2025-12-24 01:24:10 -0600

    Added routes for images in pictures.tsx file

---
+++ client/components/PicturePile.tsx
@@ -0,0 +1,11 @@
+export function PicturePile() {
+  return (
+    <div className="grid col-span-1">
+      <img 
+        src="client/images/Israel_Trejo.jpg"
+        alt="Israel Trejo"
+        className="w-[350px]"
+      />
+    </div>
+  )
+}
---
+++ server/data/pictures.tsx
@@ -0,0 +1,23 @@
+export type skill = {
+    name: string;
+    picture: string;
+};
+
+export const pictures: skill[] = [
+    {
+        name: "Israel Trejo",
+        picture: "/client/images/Israel_Trejo.jpg",
+    },
+    {
+        name: "Comic Drawings",
+        picture: "/client/images/comic_characters_design.jpg",
+    },
+    {
+        name: "live_drawing",
+        picture: "/client/images/live_drawing.jpg",
+    },
+    {
+        name: "live_drawing_2",
+        picture: "/client/images/live_drawing2.jpg",
+    }
+];
